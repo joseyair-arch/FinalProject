e1a2ac5a319d5281f08e9be4ef911f0c635d50b9
jose <jhernandezv48@miumg.edu.gt>
2026-05-19 20:36:11 -0600

    datos del libro: agregar al autor

--- src/project/package-info.java
+++ src/project/package-info.java
@@ -1,1 +1,34 @@
 package project;
+
+ class book extends Material {
+
+     private String author;
+    
+
+    public book(String author) {
+
+        
+
+        this.author=author;
+       
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String toString() {
+
+        return getTitulo()+" - "+author;
+    }
+
+	private String getTitulo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
