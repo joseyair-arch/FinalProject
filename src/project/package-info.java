418a0586dc54a5923409fa4275d84c98fb400add
jose <jhernandezv48@miumg.edu.gt>
2026-05-19 20:49:59 -0600

    datos del libro: agregar el año

--- src/project/package-info.java
+++ src/project/package-info.java
@@ -3,14 +3,14 @@ package project;
  class book extends Material {
 
      private String author;
-    
+     private int anio;
 
-    public book(String author) {
+    public book(String author,int anio) {
 
         
 
         this.author=author;
-       
+        this.anio=anio;
     }
 
     public String getAuthor() {
@@ -20,6 +20,21 @@ package project;
     public void setAuthor(String author) {
         this.author = author;
     }
+    
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+
+        if(anio<1900){
+
+            throw new IllegalArgumentException("year inválid");
+        }
+
+        this.anio=anio;
+    }
+    
 
     public String toString() {
 
